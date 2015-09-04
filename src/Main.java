e947f00e524fa7866820484d77b8e6a16961f928
Suranjana <suranjana1128@gmail.com>
2015-09-04 15:06:34 +0800

    Updated MAIN interface

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,43 @@ import java.io.*;
 
 public class Main {
 
+	private final static String SUBROUTINE = "SUBROUTINE ARCHITECTURE";
+	private final static String ADT = "ABSTRACT DATA TYPE ARCHITECTURE";
+	private final static String EXIT_MESSAGE = "SYSTEM IS EXITING.";
+	private final static String SUBROUTINE_CHOICE = "1";
+	private final static String ADT_CHOICE = "2";
+	private final static String EXIT_CHOICE = "0";
+	private final static String ERROR_MESSAGE = "The number you have chosen is not in the list of architectures.";
+
 	public static void main(String args[]) throws IOException {
-		display("Assignment 1 implements 2 types of Logical Architecture.");
-		display("1. Shared repo/subroutine with shared data");
-		display("2. Abstract Data Type");
-		display("Type 1 to access Subroutine, and 2 to access ADT.");
+		display("CS3219 Assignment 1 implements 2 types of Logical Architecture.");
+		display("1. " + SUBROUTINE);
+		display("2. " + ADT);
+		display("0. Exit System.");
+		display("Type 1 to access Subroutine, 2 to access ADT and 0 to Exit.");
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String choiceOfArchi = br.readLine();
-		if (choiceOfArchi.equals("1")) {
-			Subroutine subroutineHandler = new Subroutine();
-			subroutineHandler.run();
-		} else if (choiceOfArchi.equals("2")) {
-			AbstractDataType adtHandler = new AbstractDataType();
-			adtHandler.run();
-		} else {
-			display("The number you have chosen is not in the list of architectures. Please try again.");
+		String choiceOfArchi = EXIT_CHOICE; // default set to 0 for Exit
+		Subroutine subroutineHandler = new Subroutine();
+		AbstractDataType adtHandler = new AbstractDataType();
+
+		while (true) {
+			choiceOfArchi = br.readLine();
+			if (choiceOfArchi.equals(SUBROUTINE_CHOICE)) {
+				display(SUBROUTINE);
+				subroutineHandler.run();
+			} else if (choiceOfArchi.equals(ADT_CHOICE)) {
+				display(ADT);
+				adtHandler.run();
+			} else if (choiceOfArchi.equals(EXIT_CHOICE)) {
+				display(EXIT_MESSAGE);
+				break;
+			} else {
+				display(ERROR_MESSAGE);
+			}
+			display("Please choose another Architecture. Or type 0 to Exit.");
 		}
+
 	}
 
 	private static void display(String sentence) {
